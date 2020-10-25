f01a575c5010f3ce1b22709d03acf856a3f2e16a
Arjhun777 <arjhun777@gmail.com>
2020-10-25 14:18:21 +0530

    module added

--- .babelrc.js
+++ .babelrc.js
@@ -1,23 +1,33 @@
 module.exports = {
-    presets: [
-      [
-        '@babel/preset-env',
-        {
-          targets: {
-            node: 'current',
-          },
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current',
         },
-      ],
-    ],
-    plugins: [
-      '@babel/plugin-transform-runtime',
-      '@babel/plugin-proposal-class-properties'
-    ],
-    env: {
-      development: {
-        sourceMaps: true,
-        retainLines: true,
       },
+    ],
+  ],
+  plugins: [
+    '@babel/plugin-transform-runtime',
+    '@babel/plugin-proposal-class-properties',
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        alias: {
+          'Src': './src',
+          'Routes': './src/routes',
+          'Models': './src/models'
+        },
+      },
+    ],
+  ],
+  env: {
+    development: {
+      sourceMaps: true,
+      retainLines: true,
     },
-  };
-  
+  },
+};
