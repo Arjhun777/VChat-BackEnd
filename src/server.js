f7082c704401289b349dda440ba755541d5603bb
Arjhun777 <arjhun777@gmail.com>
2020-12-06 21:13:24 +0530

    toggle user video added for screen share

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import server from 'http';
 import socketIO from 'socket.io';
 import { v4 as uuidV4 } from 'uuid';
+// import './mailer.js';
 
 const app = express();
 const serve = server.Server(app);
@@ -22,17 +23,24 @@ io.on('connection', socket => {
     console.log('socket established')
     socket.on('join-room', (userData) => {
         const { roomID, userID } = userData;
-        console.log('Joinned in Room', roomID, 'user data', JSON.stringify(userData));
         socket.join(roomID);
         socket.to(roomID).broadcast.emit('new-user-connect', userData);
         socket.on('disconnect', () => {
             socket.to(roomID).broadcast.emit('user-disconnected', userID);
         });
         socket.on('broadcast-message', (message) => {
-            console.log('new message received', JSON.stringify(message));
             socket.to(roomID).broadcast.emit('new-broadcast-messsage', {...message, userData});
-        })
-    })
+        });
+        // socket.on('reconnect-user', () => {
+        //     socket.to(roomID).broadcast.emit('new-user-connect', userData);
+        // });
+        socket.on('display-media', (value) => {
+            socket.to(roomID).broadcast.emit('display-media', {userID, value });
+        });
+        socket.on('user-video-off', (value) => {
+            socket.to(roomID).broadcast.emit('user-video-off', value);
+        });
+    });
 });
 
 // Server listen initilized
